a9df25210dc18d66f2e73b5cfd5d10f986868403
Rajeev Rathore <rajeev100.msps@gmail.com>
2026-06-13 12:58:39 +0530

    Update database.py

--- Backend/database.py
+++ Backend/database.py
@@ -1,20 +1,40 @@
+# import os
+# from sqlalchemy import create_engine
+# from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.orm import sessionmaker
+# from dotenv import load_dotenv
+
+# # .env file se load karo (local ke liye)
+# # Render pe Environment Variables se automatically aayega
+# load_dotenv()
+
+# # MYSQL_HOST     = os.getenv("MYSQLHOST",     "localhost")
+# MYSQL_HOST     = os.getenv("MYSQLHOST",     "thomas.proxy.rlwy.net:36051")
+# MYSQL_PORT     = os.getenv("MYSQLPORT",     "3306")
+# # MYSQL_PORT     = os.getenv("MYSQLPORT",     "36051")
+# MYSQL_USER     = os.getenv("MYSQLUSER",     "root")
+# MYSQL_PASSWORD = os.getenv("MYSQLPASSWORD", "")
+# MYSQL_DATABASE = os.getenv("MYSQLDATABASE", "resume_db")
+
+# DB_URL = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}"
+
+# engine       = create_engine(DB_URL)
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# Base         = declarative_base()
+
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 
-# .env file se load karo (local ke liye)
-# Render pe Environment Variables se automatically aayega
 load_dotenv()
 
-# MYSQL_HOST     = os.getenv("MYSQLHOST",     "localhost")
-MYSQL_HOST     = os.getenv("MYSQLHOST",     "thomas.proxy.rlwy.net:36051")
-MYSQL_PORT     = os.getenv("MYSQLPORT",     "3306")
-# MYSQL_PORT     = os.getenv("MYSQLPORT",     "36051")
-MYSQL_USER     = os.getenv("MYSQLUSER",     "root")
-MYSQL_PASSWORD = os.getenv("MYSQLPASSWORD", "")
-MYSQL_DATABASE = os.getenv("MYSQLDATABASE", "resume_db")
+MYSQL_HOST     = os.getenv("DB_HOST",     "localhost")
+MYSQL_PORT     = os.getenv("DB_PORT",     "3306")
+MYSQL_USER     = os.getenv("DB_USER",     "root")
+MYSQL_PASSWORD = os.getenv("DB_PASSWORD", "")
+MYSQL_DATABASE = os.getenv("DB_NAME",     "railway")
 
 DB_URL = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}"
 
