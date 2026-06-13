1f8104efe4500b88d0fbcae3a4f32e0419f14261
Rajeev Rathore <rajeev100.msps@gmail.com>
2026-06-13 16:11:42 +0530

    Update models.py

--- Backend/models.py
+++ Backend/models.py
@@ -1,14 +1,26 @@
+# from sqlalchemy import Column, Integer, String, Float, Text
+# from database import Base
+
+# class Resume(Base):
+#     __tablename__ = "resumes"
+
+#     id            = Column(Integer, primary_key=True, index=True)
+#     filename      = Column(String(255))
+#     jd_skills     = Column(Text)
+#     resume_skills = Column(Text)
+#     match_score   = Column(Float)
+#     # matched aur missing MySQL mein manually add karo:
+#     # ALTER TABLE resumes ADD COLUMN matched TEXT;
+#     # ALTER TABLE resumes ADD COLUMN missing TEXT;
+
 from sqlalchemy import Column, Integer, String, Float, Text
 from database import Base
 
 class Resume(Base):
     __tablename__ = "resumes"
 
-    id            = Column(Integer, primary_key=True, index=True)
-    filename      = Column(String(255))
-    jd_skills     = Column(Text)
+    id = Column(Integer, primary_key=True, index=True)
+    filename = Column(String(255))
+    jd_skills = Column(Text)
     resume_skills = Column(Text)
-    match_score   = Column(Float)
-    # matched aur missing MySQL mein manually add karo:
-    # ALTER TABLE resumes ADD COLUMN matched TEXT;
-    # ALTER TABLE resumes ADD COLUMN missing TEXT;
+    match_score = Column(Float)
